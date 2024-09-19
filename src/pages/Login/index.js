4ec5ea0b1fa0e975f3d573cebb7ee73fc2ce26d4
Bruno <bitencourt.mamute@gmail.com>
2024-09-19 09:34:25 -0300

    commit bruno

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,1 +1,36 @@
-''
+import React from 'react';
+import { View, Text, TextInput, StyleSheet , TouchableOpacity} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import * as Animatable from 'react-native-animatable';
+
+export default function Login() {
+
+    return (
+        <View style={styles.container}>
+            <Animatable.View animation="fadeInLeft" style={styles.containerHeader}>
+                <Text style={styles.message}>Login</Text>
+            </Animatable.View>
+
+
+        <Animatable.View animation="fadeInUp" style={styles.containerForm}>
+                <TextInput
+                    placeholder='Insira aqui seu e-mail'
+                    style={styles.input}
+                />
+
+                <TextInput
+                placeholder='Insira aqui sua senha'
+                style={styles.input}
+                />
+
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Entrar</Text>
+                </TouchableOpacity>
+
+        </Animatable.View>
+        
+
+
+        </View>
+    );
+}
